3940abd0d81b78f2e6100225ca016b93fbce0d4f
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-05-06 18:08:14 +0200

    Added gaussian blur

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-
 /**
  * @brief Entry point of the executable
  * @param argc Number of command line arguments passed
  * @param argv List of command line arguments
- * @return Exit code of the application. 0 if Program ran successfully; other value else
-*/
+ * @return Exit code of the application. 0 if Program ran successfully; other
+ * value else
+ */
 int main(int argc, char** argv) {
-	cv::Mat emptyImg(100, 100, CV_8UC3, cv::Scalar(255, 0, 0));
-	cv::imshow("An image", emptyImg);
-	cv::waitKey();
+    cv::Mat emptyImg(100, 100, CV_8UC3, cv::Scalar(255, 0, 0));
+    cv::imshow("An image", emptyImg);
+    cv::waitKey();
 
-	cv::Mat loadedImage;
-	loadedImage = cv::imread("./opencv.png");
-	std::cout << "Image loaded" << std::endl;
-	
-	cv::imshow("An image", loadedImage);
-	cv::waitKey();
-	std::cout << "Key pressed" << std::endl;
+    cv::Mat loadedImage;
+    loadedImage = cv::imread("./opencv.png");
+    cv::Mat cropped = cv::Mat(loadedImage, cv::Rect(150, 150, 100, 100));
+    std::cout << "Image loaded" << std::endl;
 
-	cv::Mat cropped(loadedImage, cv::Rect(150, 150, 100, 100));
-	cv::imshow("An image", cropped);
-	cv::waitKey();
 
-	return 0;
+    cv::imshow("An image", loadedImage);
+    cv::waitKey();
+    std::cout << "Key pressed" << std::endl;
+
+    cv::GaussianBlur(loadedImage, loadedImage, cv::Size(101, 101), 5);
+
+    cv::imshow("An image", cropped);
+    cv::waitKey();
+
+    return 0;
 }
