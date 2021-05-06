a870e0906015b6dc15531236aaeb9767737452e7
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-05-06 14:28:09 +0200

    Added working dir, changed order

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,21 @@
  * @return Exit code of the application. 0 if Program ran successfully; other value else
 */
 int main(int argc, char** argv) {
+	cv::Mat emptyImg(100, 100, CV_8UC3, cv::Scalar(255, 0, 0));
+	cv::imshow("An image", emptyImg);
+	cv::waitKey();
+
 	cv::Mat loadedImage;
 	loadedImage = cv::imread("./opencv.png");
 	std::cout << "Image loaded" << std::endl;
-
-	cv::Mat emptyImg(100, 100, CV_8UC3, cv::Scalar(255, 0, 0));
-
-	//cv::Mat cropped(loadedImage, cv::Rect(50, 50, 20, 20));
 	
-	cv::imshow("Animage", loadedImage);
-	
+	cv::imshow("An image", loadedImage);
 	cv::waitKey();
 	std::cout << "Key pressed" << std::endl;
 
-	//cv::imshow("An image", cropped);
-
+	cv::Mat cropped(loadedImage, cv::Rect(150, 150, 100, 100));
+	cv::imshow("An image", cropped);
 	cv::waitKey();
+
 	return 0;
 }
